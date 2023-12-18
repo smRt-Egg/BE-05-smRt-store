23d4b90a5745e061e4a3f0716a5ce36d504b0e3b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-12-19 03:15:36 +0900

    [FEAT] entity 필드 추가, enum 추가

--- src/main/java/com/programmers/smrtstore/domain/user/domain/entity/User.java
+++ src/main/java/com/programmers/smrtstore/domain/user/domain/entity/User.java
@@ -1,11 +1,16 @@
 package com.programmers.smrtstore.domain.user.domain.entity;
 
+import com.programmers.smrtstore.domain.user.domain.enums.Gender;
+import com.programmers.smrtstore.domain.user.domain.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,10 +31,45 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    private String name;
-    private String email;
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
     private String nickName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String birth;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private boolean marketing_agree;
+
+    @Column(nullable = false)
+    private String thumbNail;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime deletedAt;
+
+    @Column(nullable = false)
+    private boolean isMembership;
+
     private String image;
+
+    @Column(nullable = false)
     private String phoneNum;
+
     private Long shippingInfo;
 }
---
+++ src/main/java/com/programmers/smrtstore/domain/user/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package com.programmers.smrtstore.domain.user.domain.enums;
+
+public enum Role {
+    USER, ADMIN
+}
