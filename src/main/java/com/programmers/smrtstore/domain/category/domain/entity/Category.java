4283ff3cda41bb2bab307c7f6010efac098d2b69
KyungMin Lee <hello.tidavid@gmail.com>
2023-12-21 15:12:50 +0900

    [FEAT] Category Entity 구현

---
+++ src/main/java/com/programmers/smrtstore/domain/category/domain/entity/Category.java
@@ -0,0 +1,32 @@
+package com.programmers.smrtstore.domain.category.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "value", nullable = false, updatable = false)
+    private String value;
+
+    @Builder
+    private Category(String value) {
+        this.value = value;
+    }
+}
