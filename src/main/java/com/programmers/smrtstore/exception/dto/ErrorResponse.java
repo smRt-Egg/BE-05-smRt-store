86845cac068d343ece66bd5ffd12f8a42747e610
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-12-22 16:10:12 +0900

    Feat/#33 global exception handler (#37)
    * [FEAT] 새롭게 정의할 exception class 들이 상속 받을 root exception 선언
    
    * [FEAT] ErrorCode 를 정의할 enum 선언
    
    * [FEAT] Error response 선언
    
    * [FEAT] 전역 예외 처리기 선언
    
    * [STYLE] 포맷팅

---
+++ src/main/java/com/programmers/smrtstore/exception/dto/ErrorResponse.java
@@ -0,0 +1,34 @@
+package com.programmers.smrtstore.exception.dto;
+
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int statusCode;
+    private final String statusCodeName;
+    private final String code;
+    private final String message;
+    private final String runtimeValue;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(
+        ErrorCode errorCode, String runtimeValue
+    ) {
+        return ResponseEntity
+            .status(errorCode.getHttpStatus())
+            .body(ErrorResponse.builder()
+                .statusCode(errorCode.getHttpStatus().value())
+                .statusCodeName(errorCode.getHttpStatus().name())
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .runtimeValue(runtimeValue)
+                .build()
+            );
+    }
+}
