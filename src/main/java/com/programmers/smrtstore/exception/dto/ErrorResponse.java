8382462feef41e725fac97c11e14de580a22832c
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-12-22 15:21:57 +0900

    [STYLE] 포맷팅

--- src/main/java/com/programmers/smrtstore/exception/dto/ErrorResponse.java
+++ src/main/java/com/programmers/smrtstore/exception/dto/ErrorResponse.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 @Getter
 @Builder
 public class ErrorResponse {
+
     private final LocalDateTime timestamp = LocalDateTime.now();
     private final int statusCode;
     private final String statusCodeName;
@@ -16,7 +17,9 @@ public class ErrorResponse {
     private final String message;
     private final String runtimeValue;
 
-    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode, String runtimeValue) {
+    public static ResponseEntity<ErrorResponse> toResponseEntity(
+        ErrorCode errorCode, String runtimeValue
+    ) {
         return ResponseEntity
             .status(errorCode.getHttpStatus())
             .body(ErrorResponse.builder()
