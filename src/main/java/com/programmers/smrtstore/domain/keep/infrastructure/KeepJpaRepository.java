5f6dfdfc63d5a04399c25ce4dcb3fbcf7817aba6
voidmelody <sss9073@naver.com>
2023-12-22 17:11:13 +0900

    [FEAT] KEEP Repository 구현

--- src/main/java/com/programmers/smrtstore/domain/keep/infrastructure/KeepJpaRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/keep/infrastructure/KeepJpaRepository.java
@@ -1,7 +1,19 @@
 package com.programmers.smrtstore.domain.keep.infrastructure;
 
 import com.programmers.smrtstore.domain.keep.domain.entity.Keep;
+import com.programmers.smrtstore.domain.keep.presentation.dto.res.KeepRankingResponse;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface KeepJpaRepository extends JpaRepository<Keep, Long> {
+    List<Keep> findAllByUserId(Long userId);
+
+    @Query("SELECT new com.programmers.smrtstore.domain.keep.presentation.dto.res.KeepRankingResponse(k.productId, COUNT(k.productId)) " +
+            "FROM Keep k " +
+            "GROUP BY k.productId " +
+            "ORDER BY COUNT(k.productId) DESC")
+    List<KeepRankingResponse> findTopProductIdsWithCount(Pageable pageable);
 }
