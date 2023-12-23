b42a7cf61c7d09f568bce7f3a5893775b64c3ac0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-12-24 05:47:15 +0900

    [FIX] validation 에러처리 수정

--- src/main/java/com/programmers/smrtstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/smrtstore/exception/GlobalExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.programmers.smrtstore.exception;
 
 import com.programmers.smrtstore.exception.dto.ErrorResponse;
+import com.programmers.smrtstore.exception.dto.ValidationErrorResponse;
 import com.programmers.smrtstore.exception.exceptionClass.CustomException;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,9 +26,21 @@ public class GlobalExceptionHandler {
         return ErrorResponse.toResponseEntity(e.getErrorCode(), e.getRuntimeValue());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<String> MethodArgumentNotValidException(Exception e) {
-        return ResponseEntity.status(400)
-            .body(e.getMessage());
+    @ExceptionHandler(value = {
+        BindException.class,
+        MethodArgumentNotValidException.class
+    })
+    public ResponseEntity<List<ValidationErrorResponse>> validationError(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<ValidationErrorResponse> errors = new ArrayList<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            ValidationErrorResponse error = new ValidationErrorResponse(
+                fieldError.getField(),
+                fieldError.getDefaultMessage(),
+                fieldError.getRejectedValue()
+            );
+            errors.add(error);
+        }
+        return ResponseEntity.badRequest().body(errors);
     }
 }
---
+++ src/main/java/com/programmers/smrtstore/exception/dto/ValidationErrorResponse.java
@@ -0,0 +1,12 @@
+package com.programmers.smrtstore.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ValidationErrorResponse {
+    private String field;
+    private String message;
+    private Object rejectedValue;
+}
