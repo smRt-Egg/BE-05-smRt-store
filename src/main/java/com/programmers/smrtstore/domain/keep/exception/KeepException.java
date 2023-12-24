241343dd411af1e9a8f0f7a04de012df5d3c80a8
voidmelody <sss9073@naver.com>
2023-12-24 16:31:38 +0900

    [REFACT] 에러 코드 관련 수정

--- src/main/java/com/programmers/smrtstore/domain/keep/exception/KeepException.java
+++ src/main/java/com/programmers/smrtstore/domain/keep/exception/KeepException.java
@@ -3,8 +3,8 @@ package com.programmers.smrtstore.domain.keep.exception;
 import com.programmers.smrtstore.core.properties.ErrorCode;
 import com.programmers.smrtstore.exception.exceptionClass.CustomException;
 
-public class KeepNotFoundException extends CustomException {
-    public KeepNotFoundException() {
-        super(ErrorCode.KEEP_NOT_FOUND_ERROR, ErrorCode.KEEP_NOT_FOUND_ERROR.getMessage());
+public class KeepException extends CustomException {
+    public KeepException(ErrorCode errorCode) {
+        super(errorCode, errorCode.getMessage());
     }
 }
