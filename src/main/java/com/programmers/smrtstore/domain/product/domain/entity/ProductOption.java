1832d0fc67e4264016c1a1bbf507fe26a3c99f17
KyungMin Lee <hello.tidavid@gmail.com>
2023-12-24 17:03:19 +0900

    [FEAT] Product 도메인 기능 구현

--- src/main/java/com/programmers/smrtstore/domain/product/domain/entity/ProductOption.java
+++ src/main/java/com/programmers/smrtstore/domain/product/domain/entity/ProductOption.java
@@ -1,11 +1,13 @@
 package com.programmers.smrtstore.domain.product.domain.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
@@ -17,47 +19,54 @@ import org.hibernate.annotations.CreationTimestamp;
 
 @Getter
 @Entity
-@Table(name = "option_TB")
+@Table(name = "category_option_TB")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Option {
+public class ProductOption {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "optionName", nullable = false, length = 50)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "option_tag", nullable = false)
+    private OptionTag optionTag;
+
+    @Column(name = "option_name", nullable = false, length = 50)
     private String optionName;
 
     @Column(name = "price", nullable = false)
-    private Integer price = 0;
+    private Integer price;
 
-    @Column(name = "stockQuantity", nullable = false)
+    @Column(name = "stock_quantity", nullable = false)
     private Integer stockQuantity;
 
     @CreationTimestamp
-    @Column(name = "register_date", nullable = false, updatable = false)
+    @Column(name = "register_date", nullable = false)
     private Timestamp registerDate;
 
-    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
     @Builder
-    private Option(String optionName, Integer price, Integer stockQuantity, Product product) {
+    public ProductOption(OptionTag optionTag, String optionName, Integer price,
+        Integer stockQuantity, Product product) {
+        this.optionTag = optionTag;
         this.optionName = optionName;
         this.price = price == null ? 0 : price;
         this.stockQuantity = stockQuantity;
         this.product = product;
     }
 
-    void addStockQuantity(Integer quantity) {
+    public void addStockQuantity(Integer quantity) {
         this.stockQuantity += quantity;
     }
 
-    void removeStockQuantity(Integer quantity) {
+    public void removeStockQuantity(Integer quantity) {
         if (this.stockQuantity < quantity) {
             // TODO: using Custom Exception
-            throw new IllegalArgumentException("재고가 부족합니다.");
+            throw new IllegalArgumentException();
         }
         this.stockQuantity -= quantity;
     }
