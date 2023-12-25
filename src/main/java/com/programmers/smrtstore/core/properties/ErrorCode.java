c3b85318df63ae362dd989d601ec18b97367f21a
KyungMin Lee <hello.tidavid@gmail.com>
2023-12-25 16:39:41 +0900

    [FEAT] ProductQuantity Entity 수정 및 기능 구현

--- src/main/java/com/programmers/smrtstore/core/properties/ErrorCode.java
+++ src/main/java/com/programmers/smrtstore/core/properties/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.programmers.smrtstore.core.properties;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 import lombok.AllArgsConstructor;
@@ -10,8 +11,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     // 500
-    SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생하였습니다.");
+    SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생하였습니다."),
 
+    // 400
+    PRODUCT_QUANTITY_NOT_ENOUGH(BAD_REQUEST, "상품의 재고가 부족합니다.");
     private final HttpStatus httpStatus;
     private final String message;
 
--- src/main/java/com/programmers/smrtstore/domain/product/exception/ProductException.java
+++ src/main/java/com/programmers/smrtstore/domain/product/exception/ProductException.java
@@ -5,6 +5,10 @@ import com.programmers.smrtstore.exception.exceptionClass.CustomException;
 
 public class ProductException extends CustomException {
 
+    public ProductException(ErrorCode errorCode) {
+        super(errorCode, null);
+    }
+
     public ProductException(ErrorCode errorCode,
         String runtimeValue) {
         super(errorCode, runtimeValue);
