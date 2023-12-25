67662b5c877f03e64b69b115329704568ef5b9a2
KyungMin Lee <hello.tidavid@gmail.com>
2023-12-25 17:10:16 +0900

    [FEAT] Category, Option Tag id 기반 조회 기능 추가

--- src/main/java/com/programmers/smrtstore/domain/product/domain/entity/OptionTag.java
+++ src/main/java/com/programmers/smrtstore/domain/product/domain/entity/OptionTag.java
@@ -6,10 +6,21 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OptionTag {
-    COLOR(1, "색상");
+    CHOICE(1, "선택"),
+    COLOR(2, "색상"),
+    SIZE(3, "사이즈"),
+    ERROR(-1, "ERROR");
 
     private final Integer id;
     private final String name;
 
+    public static OptionTag findById(Integer id) {
+        for (OptionTag optionTag : OptionTag.values()) {
+            if (optionTag.getId().equals(id)) {
+                return optionTag;
+            }
+        }
+        return ERROR;
+    }
 }
 
