cca52741e394846c4607819b6b346e38104fae91
YongNyeo <won05121@gmail.com>
2023-12-26 17:45:52 +0900

    [REFACT] CouponQuantity 도메인 구현

--- src/main/java/com/programmers/smrtstore/domain/coupon/domain/entity/CouponQuantity.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/domain/entity/CouponQuantity.java
@@ -1,21 +1,37 @@
 package com.programmers.smrtstore.domain.coupon.domain.entity;
 
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.coupon.domain.exception.CouponException;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Table(name = "coupon_quantity_TB")
 public class CouponQuantity {
 
     @Id
-    @Column(name = "coupon_id")
+    @Column(nullable = false)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     private Coupon coupon;
 
+
     private Integer value;
+
+    public void increase(int num) {
+        value+=num;
+    }
+
+    public void decrease(int num) {
+        if (value < num) {
+            throw new CouponException(ErrorCode.COUPON_NOT_ENOUGH, String.valueOf(value));
+        }
+        value-=num;
+    }
 }
