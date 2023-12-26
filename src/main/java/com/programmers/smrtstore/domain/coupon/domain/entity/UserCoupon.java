fde25f49c870d7ecfea645bf4ed32d26e3b61986
YongNyeo <won05121@gmail.com>
2023-12-26 15:58:48 +0900

    [REFACT] UserCoupon 도메인 구현

--- src/main/java/com/programmers/smrtstore/domain/coupon/domain/entity/UserCoupon.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/domain/entity/UserCoupon.java
@@ -1,19 +1,17 @@
 package com.programmers.smrtstore.domain.coupon.domain.entity;
 
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.coupon.domain.exception.CouponException;
 import com.programmers.smrtstore.domain.user.domain.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Table(
-        name = "coupon_owned_user_TB",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "UniqueUserAndCoupon",columnNames = {"user_id","coupon_id"})})
+@Table(name = "coupon_owned_user_TB")
 public class UserCoupon {
 
     @Id
@@ -21,11 +19,11 @@ public class UserCoupon {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "coupon_id",nullable = false)
+    @JoinColumn(name = "coupon_id", nullable = false)
     private Coupon coupon;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id",nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(nullable = false)
@@ -34,24 +32,70 @@ public class UserCoupon {
     @Column(nullable = false)
     private Integer issueCount;
 
-    @Builder
-    public UserCoupon(Coupon coupon, User user) {
+    private UserCoupon(Coupon coupon, User user) {
         this.coupon = coupon;
         this.user = user;
         this.useYn = false;
         this.issueCount = 1;
     }
 
-    public void downloadCoupon() {
+    public static UserCoupon of(Coupon coupon, User user) {
+        validCouponWithUser(coupon, user);
+        return new UserCoupon(coupon, user);
+    }
+
+    public void reIssueCoupon() {
+        validCouponWithUser(coupon, user);
+        validCouponIssueCount();
+        validExistCoupon();
+
         useYn = false;
         increaseIssueCount();
     }
+
     public void useCoupon() {
+        validCouponWithUser(coupon, user);
+        validCouponUse();
         useYn = true;
     }
 
-    public void increaseIssueCount() {
-        issueCount+=1;
+    private void increaseIssueCount() {
+        issueCount += 1;
     }
+
+    public static void validCouponWithUser(Coupon coupon, User user) {
+        coupon.validCoupon();
+        // validCustomerManageBenefitType(coupon.getCustomerManageBenefitType()
+        validMembership(coupon.isMembershipCouponYn(), user.isMembershipYN()); //멤버십 쿠폰일때 멤버십 유저인지?
+    }
+
+    private static void validMembership(boolean couponMembership, boolean userMembership) {
+        if (couponMembership && !userMembership) {
+            throw new CouponException(ErrorCode.NOT_MEMBERSHIP, String.valueOf(userMembership));
+        }
+    }
+
+//    private void validCustomerManageBenefitType(CustomerManageBenefitType type) {
+//          User.getXXX());//쿠폰에 맞는 고객인지?-> User,ORDER랑 같이 NEW,ALL,TALK 등 USER TYPE에 대한 개발 필요
+//    }
+
+    private void validExistCoupon() {
+        if (!useYn) {
+            throw new CouponException(ErrorCode.COUPON_EXIST, String.valueOf(useYn));
+        }
+    }
+
+    private void validCouponIssueCount() {
+        if (coupon.getCouponValue().getIdPerIssuableCount() <= issueCount) {
+            throw new CouponException(ErrorCode.ISSUE_COUNT_EXCEED, String.valueOf(issueCount));
+        }
+    }
+
+    private void validCouponUse() {
+        if (useYn) {
+            throw new CouponException(ErrorCode.COUPON_ALREADY_USE, String.valueOf(useYn));
+        }
+    }
+
 
 }
