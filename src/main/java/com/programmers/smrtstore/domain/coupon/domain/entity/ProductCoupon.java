3dc005d97a761d4ee837754e3904404f60d9942b
YongNyeo <won05121@gmail.com>
2023-12-26 17:45:15 +0900

    [REFACT] ProductCoupon 도메인 구현

--- src/main/java/com/programmers/smrtstore/domain/coupon/domain/entity/ProductCoupon.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/domain/entity/ProductCoupon.java
@@ -1,35 +1,38 @@
 package com.programmers.smrtstore.domain.coupon.domain.entity;
 
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.coupon.domain.exception.CouponException;
 import com.programmers.smrtstore.domain.product.domain.entity.Product;
+import com.programmers.smrtstore.domain.user.domain.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Table(
-        name = "coupon_applied_product_TB",
-        uniqueConstraints = {
-        @UniqueConstraint(name = "UniqueProductAndCoupon",columnNames = {"product_id","coupon_id"})})
+@Table(name = "coupon_applied_product_TB")
 public class ProductCoupon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "coupon_id",nullable = false)
+    @JoinColumn(name = "coupon_id", nullable = false)
     private Coupon coupon;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id",nullable = false)
-    private Product productId;
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
 
-    @Builder
-    public ProductCoupon(Coupon coupon, Product productId) {
+    private ProductCoupon(Coupon coupon, Product product) {
         this.coupon = coupon;
-        this.productId = productId;
+        this.product = product;
     }
+
+    public static ProductCoupon of(Coupon coupon, Product product) {
+        return new ProductCoupon(coupon, product);
+    }
+
 }
