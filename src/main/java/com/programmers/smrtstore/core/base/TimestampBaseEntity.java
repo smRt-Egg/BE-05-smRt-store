0278ea476b3ffdb9f12fc1de1748b461d18471de
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-12-29 00:47:25 +0900

    [FEAT] orderSheet entity 추가와 order 관련 entity 정리 (#62)
    * [FEAT] orderSheet entity 추가와 order 관련 entity 정리
    
    * [FEAT] 배송비 필드 이름 변경
    
    * [FEAT] orderSheet 가격정보 필드 삭제
    
    * [FIX] insertable 삭제
    
    * [FEAT] date 관련 base entity 추가
    
    * [FEAT] 필드 VO 변경
    
    * [FEAT] 배송 정보 entity 이름 변경 및 VO 적용
    
    * [FEAT] 배송 정보 필드 orderSheet 에서 order 로 이동
    
    * [FIX] 스네이크 케이스 카멜케이스로 변경

---
+++ src/main/java/com/programmers/smrtstore/core/base/TimestampBaseEntity.java
@@ -0,0 +1,24 @@
+package com.programmers.smrtstore.core.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@MappedSuperclass
+public abstract class TimestampBaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
--- src/main/java/com/programmers/smrtstore/domain/order/domain/entity/OrderSheet.java
+++ src/main/java/com/programmers/smrtstore/domain/order/domain/entity/OrderSheet.java
@@ -1,6 +1,7 @@
 package com.programmers.smrtstore.domain.order.domain.entity;
 
-import com.programmers.smrtstore.domain.product.domain.entity.Product;
+import com.programmers.smrtstore.domain.user.domain.entity.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,16 +10,21 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "ordered_product_TB")
+@Table(name = "order_sheet_TB")
 @Entity
-public class OrderedProduct {
+public class OrderSheet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,17 +32,14 @@ public class OrderedProduct {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="order_sheet_id")
-    private OrderSheet orderSheet;
+    @JoinColumn(name = "user_id", updatable = false)
+    private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="product_id")
-    private Product product;
+    @OneToMany(mappedBy = "orderSheet", cascade = CascadeType.ALL)
+    private List<OrderedProduct> orderedProducts;
 
-    @Column(name = "quantity")
-    private Integer quantity;
-
-    @Column(name = "total_price")
-    private Integer totalPrice;
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
 }
