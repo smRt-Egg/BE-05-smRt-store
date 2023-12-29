d1b71e12e7f732a395ea985d32fe3dabe5da07e2
YongNyeo <won05121@gmail.com>
2023-12-29 12:13:31 +0900

    [REFACT] Coupon ErrorCode 수정

--- src/main/java/com/programmers/smrtstore/core/properties/ErrorCode.java
+++ src/main/java/com/programmers/smrtstore/core/properties/ErrorCode.java
@@ -36,14 +36,14 @@ public enum ErrorCode {
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생하였습니다."),
     // 200
 
-    NON_MEMBERSHIP(OK, "멤버쉽 유저 전용 쿠폰입니다"),
-    COUPON_NOT_ENOUGH(OK, "쿠폰 재고가 부족합니다."),
-    COUPON_DATE_INVALID(OK, "쿠폰 유효 기간이 끝났습니다."),
-    COUPON_EXIST(OK, "사용 하지 않은 쿠폰이 존재합니다."),
-    ISSUE_COUNT_EXCEED(OK, "인당 쿠폰 발급 횟수를 초과했습니다."),
-    COUPON_ALREADY_USED(OK, "이미 사용 완료한 쿠폰입니다."),
-    COUPON_NOT_AVAILABLE(OK, "유효하지 않은 쿠폰입니다."),
-    ORDER_PRICE_NOT_ENOUGH(OK, "쿠폰의 최소 주문 금액 미만입니다."),
+    NON_MEMBERSHIP(BAD_REQUEST, "멤버쉽 유저 전용 쿠폰입니다"),
+    COUPON_NOT_ENOUGH(BAD_REQUEST, "쿠폰 재고가 부족합니다."),
+    COUPON_DATE_INVALID(BAD_REQUEST, "쿠폰 유효 기간이 끝났습니다."),
+    COUPON_EXIST(BAD_REQUEST, "사용 하지 않은 쿠폰이 존재합니다."),
+    ISSUE_COUNT_EXCEED(BAD_REQUEST, "인당 쿠폰 발급 횟수를 초과했습니다."),
+    COUPON_ALREADY_USED(BAD_REQUEST, "이미 사용 완료한 쿠폰입니다."),
+    COUPON_NOT_AVAILABLE(BAD_REQUEST, "유효하지 않은 쿠폰입니다."),
+    ORDER_PRICE_NOT_ENOUGH(BAD_REQUEST, "쿠폰의 최소 주문 금액 미만입니다."),
     COUPON_NOT_FOUND(NOT_FOUND, "존재하지 않는 쿠폰번호입니다.");
 
     private final HttpStatus httpStatus;
