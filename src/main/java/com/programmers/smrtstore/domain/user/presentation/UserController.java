831291055c915f4b44f48962bcdcb82caa5c8184
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-12-30 03:16:43 +0900

    [FIX] 엔드포인트 위치 변경

--- src/main/java/com/programmers/smrtstore/domain/user/presentation/UserController.java
+++ src/main/java/com/programmers/smrtstore/domain/user/presentation/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth/user")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
