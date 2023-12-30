46fd936b443433cc7f39652f759f29e6541fbfe9
YongNyeo <won05121@gmail.com>
2023-12-31 02:24:16 +0900

    [FEAT] User 전용 CouponDto 추가

--- src/main/java/com/programmers/smrtstore/domain/coupon/presentation/res/UserCouponResponse.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/presentation/res/UserCouponResponse.java
@@ -1,10 +1,31 @@
 package com.programmers.smrtstore.domain.coupon.presentation.res;
 
+import com.programmers.smrtstore.domain.coupon.domain.entity.Coupon;
+import com.programmers.smrtstore.domain.coupon.domain.entity.enums.BenefitUnitType;
+import com.programmers.smrtstore.domain.coupon.domain.entity.enums.CouponType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class UserCouponResponse {
-    //coupon id
-    //coupon name
-    //coupon discount type
-    //coupon discount value
-    //coupon valid end date
-    //coupon duplicate yn
+
+    private final String name;
+    private final BenefitUnitType type;
+    private final Long discountValue;
+    private final Long minOrderPrice;
+    private final boolean isDupilcate;
+    private final LocalDateTime validEndDate;
+    public static UserCouponResponse toDto(Coupon coupon) {
+        return new UserCouponResponse(
+                coupon.getCouponValue().getName(),
+                coupon.getBenefitUnitType(),
+                coupon.getCouponValue().getMaxDiscountValue(),
+                coupon.getCouponValue().getMinOrderPrice(),
+                coupon.isDuplicationYn(),
+                coupon.getValidPeriodEndDate());
+    }
+
 }
