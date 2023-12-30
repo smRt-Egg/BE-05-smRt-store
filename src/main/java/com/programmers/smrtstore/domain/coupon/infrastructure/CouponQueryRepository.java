ebbfab371b248aabbcfce7e4d7b575bb138b756b
YongNyeo <won05121@gmail.com>
2023-12-31 02:23:44 +0900

    [FEAT] CouponQueryRepository 내 유저의 쿠폰 정보 메서드 추가

--- src/main/java/com/programmers/smrtstore/domain/coupon/infrastructure/CouponQueryRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/infrastructure/CouponQueryRepository.java
@@ -12,6 +12,7 @@ import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +34,45 @@ public class CouponQueryRepository {
                 .fetchOne();
     }
 
+    public List<Coupon> findUserCoupons(Long userId) {
+        QCouponAvailableUser qCouponAvailableUser = QCouponAvailableUser.couponAvailableUser;
+        QCoupon qCoupon = QCoupon.coupon;
 
+        return queryFactory
+                .select(qCoupon)
+                .from(qCouponAvailableUser)
+                .join(qCoupon)
+                .on(qCouponAvailableUser.coupon.id.eq(qCoupon.id))
+                .where(qCouponAvailableUser.user.id.eq(userId))
+                .fetch();
+    }
+
+    public Optional<Coupon> findCouponByUserIdAndCouponId(Long userId, Long couponId) {
+        QCouponAvailableUser qCouponAvailableUser = QCouponAvailableUser.couponAvailableUser;
+        QCoupon qCoupon = QCoupon.coupon;
+
+        Coupon coupon = queryFactory
+                .select(qCoupon)
+                .from(qCouponAvailableUser)
+                .join(qCoupon)
+                .on(qCouponAvailableUser.coupon.id.eq(qCoupon.id))
+                .where(qCouponAvailableUser.user.id.eq(userId))
+                .where(qCouponAvailableUser.coupon.id.eq(couponId))
+                .fetchOne();
+        return Optional.ofNullable(coupon);
+    }
+
+    public Long findUserCouponCount(Long userId) {
+        QCouponAvailableUser qCouponAvailableUser = QCouponAvailableUser.couponAvailableUser;
+        QCoupon qCoupon = QCoupon.coupon;
+
+        return queryFactory
+                .select(qCoupon)
+                .from(qCouponAvailableUser)
+                .join(qCoupon)
+                .on(qCouponAvailableUser.coupon.id.eq(qCoupon.id))
+                .where(qCouponAvailableUser.user.id.eq(userId))
+                .fetchCount();
+    }
 
 }
