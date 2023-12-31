56de55a4884653e56daf33c56fa25c12d2e86cec
happyjamy <78072370+happyjamy@users.noreply.github.com>
2024-01-01 04:15:52 +0900

    [FEAT] orderId 로 orderResponse 반환하는 기능 구현

--- src/main/java/com/programmers/smrtstore/domain/order/infrastructure/OrderJpaRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/order/infrastructure/OrderJpaRepository.java
@@ -1,8 +1,12 @@
 package com.programmers.smrtstore.domain.order.infrastructure;
 
 import com.programmers.smrtstore.domain.order.domain.entity.Order;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderJpaRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
+    @EntityGraph(attributePaths = {"orderSheet", "orderSheet.user"})
+    Optional<Order> findByIdWithOrderSheetAndUser(Long orderId);
 }
