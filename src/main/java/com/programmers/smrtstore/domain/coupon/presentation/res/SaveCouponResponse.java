c688925e05b496a223c44f6a42ea729d7df88619
KyungMin Lee <hello.tidavid@gmail.com>
2023-12-31 21:19:10 +0900

    Revert "Feat/#59 coupon download" (#96)

--- src/main/java/com/programmers/smrtstore/domain/coupon/presentation/res/SaveCouponResponse.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/presentation/res/SaveCouponResponse.java
@@ -1,14 +1,4 @@
 package com.programmers.smrtstore.domain.coupon.presentation.res;
 
-import com.programmers.smrtstore.domain.coupon.domain.entity.CouponAvailableUser;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class SaveCouponResponse {
-    private final Long couponId;
-    public static SaveCouponResponse toDto(CouponAvailableUser couponAvailableUser){
-        return new SaveCouponResponse(couponAvailableUser.getCoupon().getId());
-    }
 }
