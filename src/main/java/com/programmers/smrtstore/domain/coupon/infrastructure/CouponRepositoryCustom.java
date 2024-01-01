7bc32ba153a2aa783584ce935bd1e24ad5a8728c
YongNyeo <won05121@gmail.com>
2024-01-01 22:47:06 +0900

    [FEAT] customRepository 구현

---
+++ src/main/java/com/programmers/smrtstore/domain/coupon/infrastructure/CouponRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.programmers.smrtstore.domain.coupon.infrastructure;
+
+import com.programmers.smrtstore.domain.coupon.domain.entity.Coupon;
+import com.programmers.smrtstore.domain.coupon.domain.entity.CouponQuantity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CouponRepositoryCustom {
+    CouponQuantity findCouponQuantity(Long couponId);
+    List<Coupon> findUserCoupons(Long userId);
+    Optional<Coupon> findCouponByUserIdAndCouponId(Long userId, Long couponId);
+    Long findUserCouponCount(Long userId);
+}
