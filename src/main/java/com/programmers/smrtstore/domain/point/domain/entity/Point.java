ac979a3c840e153d0d7610eaa181bb506e0e709a
JIN-076 <andy230@naver.com>
2024-01-01 16:26:17 +0900

    [FEAT] 테이블몇 수정 및 Point 생성을 위한 빌더 추가

--- src/main/java/com/programmers/smrtstore/domain/point/domain/entity/Point.java
+++ src/main/java/com/programmers/smrtstore/domain/point/domain/entity/Point.java
@@ -1,5 +1,6 @@
 package com.programmers.smrtstore.domain.point.domain.entity;
 
+import com.programmers.smrtstore.domain.point.domain.entity.enums.PointStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,13 +12,13 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.time.LocalDateTime;
-import java.util.Date;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "point")
+@Table(name = "point_TB")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Point {
@@ -49,5 +50,19 @@ public class Point {
     private LocalDateTime expiredAt;
 
     @Column(name = "membership_apply_yn", nullable = false)
-    private Boolean memberShipApplyYn;
+    private Boolean membershipApplyYn;
+
+    @Builder
+    private Point(Long userId, Long orderId, PointStatus pointStatus, int pointValue, Boolean membershipApplyYn) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.pointStatus = pointStatus;
+        this.pointValue = pointValue;
+        this.issuedAt = LocalDateTime.now();
+        this.expiredAt = !pointStatus.equals(PointStatus.ACCUMULATED) ? null : issuedAt.plusYears(10)
+            .withHour(0)
+            .withMinute(0)
+            .withSecond(0);
+        this.membershipApplyYn = membershipApplyYn;
+    }
 }
