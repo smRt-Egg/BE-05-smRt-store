701ecaf7fe5ee560054d5bdb9da8007b35205f39
JIN-076 <andy230@naver.com>
2024-01-02 17:29:40 +0900

    [FEAT] 포인트 Dto 추가 및 Entity 변환 로직, 포인트 유효검증 로직 구현

---
+++ src/main/java/com/programmers/smrtstore/domain/point/application/dto/req/PointRequest.java
@@ -0,0 +1,44 @@
+package com.programmers.smrtstore.domain.point.application.dto.req;
+
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.point.domain.entity.Point;
+import com.programmers.smrtstore.domain.point.domain.entity.enums.PointStatus;
+import com.programmers.smrtstore.domain.point.exception.PointException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PointRequest {
+
+    private Long userId;
+    private Long orderId;
+
+    public Point toEntity(PointStatus pointStatus, Integer pointValue, boolean membershipApplyYn) {
+        validatePointValue(pointStatus, pointValue);
+        return Point.builder()
+            .userId(userId)
+            .orderId(orderId)
+            .pointStatus(pointStatus)
+            .pointValue(pointValue)
+            .membershipApplyYn(membershipApplyYn)
+            .build();
+    }
+
+    private static void validatePointValue(PointStatus pointStatus, Integer pointValue) {
+        if (pointStatus.equals(PointStatus.ACCUMULATED) || pointStatus.equals(PointStatus.USE_CANCELED)) {
+            if (pointValue < 0) {
+                throw new PointException(ErrorCode.POINT_ILLEGAL_ARGUMENT, String.valueOf(pointValue));
+            }
+        } else {
+            if (pointValue > 0) {
+                throw new PointException(ErrorCode.POINT_ILLEGAL_ARGUMENT, String.valueOf(pointValue));
+            }
+        }
+    }
+}
