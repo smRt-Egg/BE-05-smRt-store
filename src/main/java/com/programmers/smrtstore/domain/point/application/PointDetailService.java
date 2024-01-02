6f1e9a59567daaec705465325125afed349b22ae
JIN-076 <andy230@naver.com>
2024-01-02 18:36:30 +0900

    [FEAT] 요청한 사용자 검증 로직 및 포인트 상세 이력 테이블에서 originAcmId를 기준으로 GroupBy, 오름차순된 레코드를 가져와 적립된 액수별 포인트 차감 이력 저장 구현

--- src/main/java/com/programmers/smrtstore/domain/point/application/PointDetailService.java
+++ src/main/java/com/programmers/smrtstore/domain/point/application/PointDetailService.java
@@ -1,13 +1,83 @@
 package com.programmers.smrtstore.domain.point.application;
 
 
-import com.programmers.smrtstore.domain.point.presentation.dto.req.PointDetailRequest;
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.point.domain.entity.Point;
+import com.programmers.smrtstore.domain.point.domain.entity.PointDetail;
+import com.programmers.smrtstore.domain.point.exception.PointException;
+import com.programmers.smrtstore.domain.point.infrastructure.PointDetailJpaRepository;
+import com.programmers.smrtstore.domain.point.infrastructure.PointJpaRepository;
+import com.programmers.smrtstore.domain.point.application.dto.req.PointDetailRequest;
+import com.programmers.smrtstore.domain.point.application.dto.res.PointDetailCustomResponse;
+import com.programmers.smrtstore.domain.user.domain.entity.User;
+import com.programmers.smrtstore.domain.user.exception.UserException;
+import com.programmers.smrtstore.domain.user.infrastructure.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PointDetailService {
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PointDetailService {
 
-    Long saveEarnHistory(PointDetailRequest request);
-    Long saveUseHistory(PointDetailRequest request);
-    Long saveEarnCancelHistory(PointDetailRequest request);
-    Long saveUseCancelHistory(PointDetailRequest request);
-    Long saveExpirationHistory(Long userId);
+    private final UserRepository userRepository;
+    private final PointJpaRepository pointRepository;
+    private final PointDetailJpaRepository pointDetailRepository;
+
+    private Point getPointById(Long pointId) {
+        return pointRepository.findById(pointId)
+            .orElseThrow(() -> new PointException(ErrorCode.POINT_NOT_FOUND, String.valueOf(pointId)));
+    }
+
+    public Long saveAccumulationHistory(PointDetailRequest request) {
+        return null;
+    }
+
+    private User validateUserExists(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND, String.valueOf(userId)));
+    }
+
+    public Long saveAccumulationCancelHistory(PointDetailRequest request) {
+        return null;
+    }
+
+    public Long saveUseHistory(PointDetailRequest request) {
+
+        validateUserExists(request.getUserId());
+
+        Long pointId = request.getPointId();
+        Long userId = request.getUserId();
+
+        List<PointDetailCustomResponse> history = pointDetailRepository.getSumGroupByOriginAcmId(userId);
+
+        Point point = getPointById(pointId);
+        int usedPoint = Math.abs(point.getPointValue());
+
+        Long pointDetailId = null;
+        for (PointDetailCustomResponse response : history) {
+            while (usedPoint != 0) {
+                int pointAmount = calculateDeductedPoint(response.getPointAmount(), usedPoint);
+                PointDetail pointDetail = request.toEntity(-1 * pointAmount, response.getOriginAcmId());
+                pointDetailRepository.save(pointDetail);
+                usedPoint -= pointAmount;
+                if (pointDetailId == null) pointDetailId = pointDetail.getId();
+            }
+        }
+        return pointDetailId;
+    }
+
+    private int calculateDeductedPoint(int pointAmount, int usedPoint) {
+        return Math.min(usedPoint, pointAmount);
+    }
+
+    public Long saveUseCancelHistory(PointDetailRequest request) {
+        return null;
+    }
+
+    public Long saveExpirationHistory(Long userId) {
+        return null;
+    }
 }
