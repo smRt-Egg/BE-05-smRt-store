ee2210c3a8f87cc7e3d39575d9f382f4a37b1ace
JIN-076 <andy230@naver.com>
2024-01-03 01:38:28 +0900

    [FEAT] 포인트 상세 Dto 추가 및 Entity 변환 로직 추가

---
+++ src/main/java/com/programmers/smrtstore/domain/point/application/dto/req/PointDetailRequest.java
@@ -0,0 +1,27 @@
+package com.programmers.smrtstore.domain.point.application.dto.req;
+
+import com.programmers.smrtstore.domain.point.domain.entity.PointDetail;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PointDetailRequest {
+
+    private Long userId;
+    private Long pointId;
+
+    public PointDetail toEntity(Integer pointAmount, Long originAcmId) {
+        return PointDetail.builder()
+            .pointId(pointId)
+            .userId(userId)
+            .pointAmount(pointAmount)
+            .originAcmId(originAcmId)
+            .build();
+    }
+}
