c458b3ea2402764d90adc9d5e4161c91b07eecc9
JIN-076 <andy230@naver.com>
2024-01-03 01:52:07 +0900

    [FEAT] pointId와 pointStatus에 대한 Point 조회 로직

---
+++ src/main/java/com/programmers/smrtstore/domain/point/infrastructure/PointRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.programmers.smrtstore.domain.point.infrastructure;
+
+import com.programmers.smrtstore.domain.point.domain.entity.enums.PointStatus;
+import com.programmers.smrtstore.domain.point.application.dto.res.PointResponse;
+import java.util.Optional;
+
+public interface PointRepositoryCustom {
+
+    Optional<PointResponse> findByPointIdAndPointStatus(Long pointId, PointStatus pointStatus);
+}
