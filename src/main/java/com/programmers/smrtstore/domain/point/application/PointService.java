acd4001b19eba645489efddfdb6a42c2caeee5ae
JIN-076 <andy230@naver.com>
2024-01-02 17:59:40 +0900

    [FEAT] 요청한 사용자에 대한 검증 로직 및 포인트 '사용'에 대한 포인트 이력 저장 로직 구현

--- src/main/java/com/programmers/smrtstore/domain/point/application/PointService.java
+++ src/main/java/com/programmers/smrtstore/domain/point/application/PointService.java
@@ -1,17 +1,67 @@
 package com.programmers.smrtstore.domain.point.application;
 
-import com.programmers.smrtstore.domain.point.presentation.dto.req.FindPointByIssuedDateRequest;
-import com.programmers.smrtstore.domain.point.presentation.dto.req.PointRequest;
-import com.programmers.smrtstore.domain.point.presentation.dto.req.UsePointRequest;
-import com.programmers.smrtstore.domain.point.presentation.dto.res.PointResponse;
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.point.application.dto.req.PointRequest;
+import com.programmers.smrtstore.domain.point.application.dto.res.PointResponse;
+import com.programmers.smrtstore.domain.point.domain.entity.Point;
+import com.programmers.smrtstore.domain.point.exception.PointException;
+import com.programmers.smrtstore.domain.point.infrastructure.PointJpaRepository;
+import com.programmers.smrtstore.domain.point.application.dto.req.UsePointRequest;
+import com.programmers.smrtstore.domain.user.domain.entity.User;
+import com.programmers.smrtstore.domain.user.exception.UserException;
+import com.programmers.smrtstore.domain.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PointService {
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PointService {
 
-    PointResponse earnPoint(PointRequest request);
-    PointResponse usePoint(UsePointRequest request);
-    PointResponse cancelEarnedPoint(PointRequest request);
-    PointResponse cancelUsedPoint(PointRequest request);
-    PointResponse expirePoint(Long userId);
-    PointResponse getPointByUserAndIssuedDate(FindPointByIssuedDateRequest request);
-    PointResponse getPointHistory(Long userId);
+    private final UserRepository userRepository;
+    private final PointJpaRepository pointRepository;
+
+    public static final int MAX_AVAILALBE_USE_POINT = 2000000;
+
+    @Transactional(readOnly = true)
+    public PointResponse getPointById(Long pointId) {
+        Point point = pointRepository.findById(pointId)
+            .orElseThrow(() -> new PointException(ErrorCode.POINT_NOT_FOUND));
+        return PointResponse.from(point);
+    }
+
+    public PointResponse accumulatePoint(PointRequest request) {
+        return null;
+    }
+
+    private User validateUserExists(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND, String.valueOf(userId)));
+    }
+
+    public PointResponse cancelAccumulatedPoint(PointRequest request) {
+        return null;
+    }
+
+    public PointResponse usePoint(UsePointRequest request) {
+
+        User user = validateUserExists(request.getUserId());
+
+        Point point = request.toEntity(user.isMembershipYN());
+        pointRepository.save(point);
+        return PointResponse.from(point);
+    }
+
+    public PointResponse cancelUsedPoint(PointRequest request) {
+        return null;
+    }
+
+    public PointResponse expirePoint(Long userId) {
+        return null;
+    }
+
+    public PointResponse getPointHistory(Long userId) {
+        return null;
+    }
 }
