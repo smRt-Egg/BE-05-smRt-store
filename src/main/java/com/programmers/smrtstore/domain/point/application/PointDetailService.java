38cdf9c2cb7de5af34d812951224f30fc88800ac
JIN-076 <andy230@naver.com>
2024-01-03 01:57:54 +0900

    [FEAT] 요청 사용자 검증 로직, 주문 취소 상세 이력 저장 로직 구현

--- src/main/java/com/programmers/smrtstore/domain/point/application/PointDetailService.java
+++ src/main/java/com/programmers/smrtstore/domain/point/application/PointDetailService.java
@@ -1,13 +1,71 @@
 package com.programmers.smrtstore.domain.point.application;
 
 
-import com.programmers.smrtstore.domain.point.presentation.dto.req.PointDetailRequest;
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.domain.point.application.dto.req.PointDetailRequest;
+import com.programmers.smrtstore.domain.point.application.dto.res.PointResponse;
+import com.programmers.smrtstore.domain.point.domain.entity.Point;
+import com.programmers.smrtstore.domain.point.domain.entity.PointDetail;
+import com.programmers.smrtstore.domain.point.domain.entity.enums.PointStatus;
+import com.programmers.smrtstore.domain.point.exception.PointException;
+import com.programmers.smrtstore.domain.point.infrastructure.PointDetailJpaRepository;
+import com.programmers.smrtstore.domain.point.infrastructure.PointJpaRepository;
+import com.programmers.smrtstore.domain.user.domain.entity.User;
+import com.programmers.smrtstore.domain.user.exception.UserException;
+import com.programmers.smrtstore.domain.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PointDetailService {
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PointDetailService {
 
-    Long saveEarnHistory(PointDetailRequest request);
-    Long saveUseHistory(PointDetailRequest request);
-    Long saveEarnCancelHistory(PointDetailRequest request);
-    Long saveUseCancelHistory(PointDetailRequest request);
-    Long saveExpirationHistory(Long userId);
+    private final UserRepository userRepository;
+    private final PointJpaRepository pointRepository;
+    private final PointDetailJpaRepository pointDetailRepository;
+
+    private Point getPointById(Long pointId) {
+        return pointRepository.findById(pointId)
+            .orElseThrow(() -> new PointException(ErrorCode.POINT_NOT_FOUND, String.valueOf(pointId)));
+    }
+
+    public Long saveAccumulationHistory(PointDetailRequest request) {
+        return null;
+    }
+
+    private User validateUserExists(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND, String.valueOf(userId)));
+    }
+
+    public PointResponse getByPointIdAndStatus(Long pointId, PointStatus pointStatus) {
+        return pointRepository.findByPointIdAndPointStatus(pointId, pointStatus)
+            .orElseThrow(() -> new PointException(ErrorCode.POINT_NOT_FOUND));
+    }
+
+    public Long saveAccumulationCancelHistory(PointDetailRequest request) {
+
+        validateUserExists(request.getUserId());
+
+        Long pointId = request.getPointId();
+        PointResponse pointResponse = getByPointIdAndStatus(pointId, PointStatus.ACCUMULATED);
+
+        PointDetail pointDetail = request.toEntity(pointResponse.getPointValue(), pointResponse.getId());
+        pointDetailRepository.save(pointDetail);
+        return pointDetail.getPointId();
+    }
+
+    public Long saveUseHistory(PointDetailRequest request) {
+        return null;
+    }
+
+    public Long saveUseCancelHistory(PointDetailRequest request) {
+        return null;
+    }
+
+    public Long saveExpirationHistory(Long userId) {
+        return null;
+    }
 }
