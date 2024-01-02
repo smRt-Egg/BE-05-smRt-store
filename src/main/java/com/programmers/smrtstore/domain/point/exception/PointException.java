0e78ca176432f0f9e4f1e078df5b1cd5ec65cb5f
JIN-076 <andy230@naver.com>
2024-01-02 11:55:21 +0900

    [FEAT] Point에 대한 CustomException 추가 및 단일 생성자 방식 수정

---
+++ src/main/java/com/programmers/smrtstore/domain/point/exception/PointException.java
@@ -0,0 +1,15 @@
+package com.programmers.smrtstore.domain.point.exception;
+
+import com.programmers.smrtstore.core.properties.ErrorCode;
+import com.programmers.smrtstore.exception.exceptionClass.CustomException;
+
+public class PointException extends CustomException {
+
+    public PointException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public PointException(ErrorCode errorCode, String runtimeValue) {
+        super(errorCode, runtimeValue);
+    }
+}
