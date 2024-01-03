38a100b86c960a6035404d4a9ed75e36b654fcef
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-03 10:53:28 +0900

    [FIX] OptionTag 사용 방식 변경

--- src/main/java/com/programmers/smrtstore/domain/product/domain/entity/enums/OptionTag.java
+++ src/main/java/com/programmers/smrtstore/domain/product/domain/entity/enums/OptionTag.java
@@ -1,30 +1,7 @@
 package com.programmers.smrtstore.domain.product.domain.entity.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum OptionTag {
-
-    CHOICE(1, "선택"),
-    COLOR(2, "색상"),
-    SIZE(3, "사이즈"),
-    ERROR(-1, "ERROR");
-
-
-    private final Integer id;
-    private final String name;
-
-
-    public static OptionTag findById(Integer id) {
-        for (OptionTag optionTag : OptionTag.values()) {
-            if (optionTag.getId().equals(id)) {
-                return optionTag;
-            }
-        }
-        return ERROR;
-    }
-
+    SINGLE,         // 단일 옵션 <- 옵션 없는 상품의 경우
+    COMBINATION     // 조합 옵션
 }
 
