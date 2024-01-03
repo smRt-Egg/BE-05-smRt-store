6c04cfecb6fafef8e80c17328b3e5f88706b948e
JIN-076 <andy230@naver.com>
2024-01-03 18:51:00 +0900

    [FEAT] JPQL를 이용해 pointId로 '사용' 상태의 포인트 상세 이력 조회 로직 구현

--- src/main/java/com/programmers/smrtstore/domain/point/infrastructure/PointDetailJpaRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/point/infrastructure/PointDetailJpaRepository.java
@@ -1,8 +1,13 @@
 package com.programmers.smrtstore.domain.point.infrastructure;
 
 import com.programmers.smrtstore.domain.point.domain.entity.PointDetail;
+import com.programmers.smrtstore.domain.point.application.dto.res.PointDetailResponse;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PointDetailJpaRepository extends JpaRepository<PointDetail, Long> {
+public interface PointDetailJpaRepository extends JpaRepository<PointDetail, Long>, PointDetailRepositoryCustom {
 
+    @Query("SELECT pd FROM PointDetail pd WHERE pd.pointId = :pointId")
+    List<PointDetailResponse> findUsedDetailByPointId(Long pointId);
 }
