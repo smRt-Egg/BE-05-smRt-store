0dddfbc7f5fab59badb03f8e0828144a227ba88f
JIN-076 <andy230@naver.com>
2024-01-03 18:53:48 +0900

    [FEAT] Point와 PointDetail 서비스에서 재사용되는 조회 로직을 Facade 패턴을 적용해서 인터페이스와 구현체로 구현

---
+++ src/main/java/com/programmers/smrtstore/domain/point/application/PointFacade.java
@@ -0,0 +1,16 @@
+package com.programmers.smrtstore.domain.point.application;
+
+import com.programmers.smrtstore.domain.point.application.dto.res.PointResponse;
+import com.programmers.smrtstore.domain.point.domain.entity.enums.PointStatus;
+import com.programmers.smrtstore.domain.point.application.dto.res.PointDetailResponse;
+import java.util.List;
+
+public interface PointFacade {
+
+    PointResponse getPointById(Long pointId);
+    PointResponse getByOrderIdAndStatus(Long orderId, PointStatus pointStatus);
+    List<PointDetailResponse> getUsedDetailByPointId(Long pointId);
+    List<PointDetailResponse> getUsedDetailByOrderId(Long orderId);
+    boolean validateExpiredAt(PointResponse pointResponse);
+
+}
