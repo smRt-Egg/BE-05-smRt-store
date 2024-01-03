8cf95443058ba1d9d55bd5d2ada569feeb9cde0b
voidmelody <sss9073@naver.com>
2024-01-03 22:23:07 +0900

    [FEAT] @UserId 적용

--- src/main/java/com/programmers/smrtstore/domain/qna/presentation/controller/ProductQnAController.java
+++ src/main/java/com/programmers/smrtstore/domain/qna/presentation/controller/ProductQnAController.java
@@ -1,5 +1,6 @@
 package com.programmers.smrtstore.domain.qna.presentation.controller;
 
+import com.programmers.smrtstore.common.annotation.UserId;
 import com.programmers.smrtstore.domain.qna.application.ProductQnAService;
 import com.programmers.smrtstore.domain.qna.presentation.dto.req.CreateAnswerRequest;
 import com.programmers.smrtstore.domain.qna.presentation.dto.req.CreateQuestionRequest;
@@ -19,49 +20,49 @@ public class ProductQnAController {
     private final ProductQnAService qnAService;
 
     @PostMapping("/qna")
-    public ResponseEntity<CreateQuestionResponse> createQuestion(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<CreateQuestionResponse> createQuestion(@UserId Long userId,
                                                                  @RequestBody CreateQuestionRequest request) {
         CreateQuestionResponse response = qnAService.createQuestion(userId, request);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/my/qna")
-    public ResponseEntity<List<QuestionResponse>> findMyQuestion(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<List<QuestionResponse>> findMyQuestion(@UserId Long userId,
                                                                  @RequestBody FindQuestionRequest request) {
         List<QuestionResponse> response = qnAService.findByUserId(userId, request);
         return ResponseEntity.ok(response);
     }
 
     @PutMapping("/qna")
-    public ResponseEntity<UpdateQuestionResponse> updateQuestion(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<UpdateQuestionResponse> updateQuestion(@UserId Long userId,
                                                                  @RequestBody UpdateQuestionRequest request) {
         UpdateQuestionResponse response = qnAService.updateQuestion(userId, request);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/qna/{id}")
-    public ResponseEntity<DeleteQuestionResponse> deleteQuestion(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<DeleteQuestionResponse> deleteQuestion(@UserId Long userId,
                                                                  @PathVariable("id") Long deleteId) {
         DeleteQuestionResponse response = qnAService.deleteQuestion(userId, deleteId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/products/{productId}/qna")
-    public ResponseEntity<List<QuestionResponse>> getQuestionByProductId(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<List<QuestionResponse>> getQuestionByProductId(@UserId Long userId,
                                                                          @PathVariable("productId") Long productId) {
         List<QuestionResponse> response = qnAService.findByProductId(userId, productId);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/admin/qna")
-    public ResponseEntity<AnswerResponse> addAnswerToQuestion(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<AnswerResponse> addAnswerToQuestion(@UserId Long userId,
                                                               @RequestBody CreateAnswerRequest request) {
         AnswerResponse response = qnAService.addAnswer(userId, request);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/qna/replies/{questionId}")
-    public ResponseEntity<List<AnswerResponse>> getAnswersByQuestionId(@RequestAttribute(value = "userId") Long userId,
+    public ResponseEntity<List<AnswerResponse>> getAnswersByQuestionId(@UserId Long userId,
                                                                        @PathVariable Long questionId) {
         List<AnswerResponse> response = qnAService.getAnswersByQuestionId(userId, questionId);
         return ResponseEntity.ok(response);
