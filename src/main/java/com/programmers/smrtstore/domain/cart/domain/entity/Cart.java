cd77ce46011173e4436ff56143bf343aa410e053
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-07 01:12:44 +0900

    [FEAT] Cart 엔티티 productDetailOptionId 필드 추가

--- src/main/java/com/programmers/smrtstore/domain/cart/domain/entity/Cart.java
+++ src/main/java/com/programmers/smrtstore/domain/cart/domain/entity/Cart.java
@@ -3,6 +3,7 @@ package com.programmers.smrtstore.domain.cart.domain.entity;
 import com.programmers.smrtstore.core.properties.ErrorCode;
 import com.programmers.smrtstore.domain.cart.exception.CartException;
 import com.programmers.smrtstore.domain.product.domain.entity.Product;
+import com.programmers.smrtstore.domain.product.domain.entity.ProductDetailOption;
 import com.programmers.smrtstore.domain.user.domain.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -43,6 +44,9 @@ public class Cart {
     @JoinColumn(name = "product_id")
     private Product product;
 
+    @Column(name = "product_detail_option_id", nullable = false)
+    private Long productDetailOptionId;
+
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
 
@@ -58,20 +62,24 @@ public class Cart {
     private LocalDateTime updatedAt;
 
     @Builder
-    private Cart(User user, Product product, Integer quantity) {
+    private Cart(User user, Product product, ProductDetailOption detailOption, Integer quantity) {
         this.user = user;
         this.product = product;
+        if(!detailOption.getProduct().getId().equals(product.getId())) {
+            throw new CartException(ErrorCode.CART_PRODUCT_DETAIL_OPTION_NOT_MATCH);
+        }
+        this.productDetailOptionId = detailOption.getId();
         this.quantity = quantity;
-        this.price = calculatePrice(product, quantity);
+        this.price = calculatePrice(product, detailOption.getId(), quantity);
     }
 
-    private static Integer calculatePrice(Product product, Integer quantity) {
-        return product.getSalePrice() * quantity;
+    private static Integer calculatePrice(Product product, Long detailOptionId, Integer quantity) {
+        return product.getSalePrice(detailOptionId) * quantity;
     }
 
     public void addQuantity(Integer quantity) {
         this.quantity += quantity;
-        this.price = calculatePrice(product, this.quantity);
+        this.price = calculatePrice(product, productDetailOptionId, this.quantity);
     }
 
     public void removeQuantity(Integer quantity) {
@@ -79,7 +87,7 @@ public class Cart {
             throw new CartException(ErrorCode.CART_QUANTITY_NOT_ENOUGH);
         }
         this.quantity -= quantity;
-        this.price = calculatePrice(product, this.quantity);
+        this.price = calculatePrice(product, productDetailOptionId, this.quantity);
     }
 
 }
