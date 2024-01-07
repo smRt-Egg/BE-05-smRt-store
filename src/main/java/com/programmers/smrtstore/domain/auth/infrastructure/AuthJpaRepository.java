388b29fd9b31814caa25d058b4db8452597c04bf
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-08 01:37:32 +0900

    [FIX] deletedAt 포함 Query 수정 (#158)

--- src/main/java/com/programmers/smrtstore/domain/auth/infrastructure/AuthJpaRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/auth/infrastructure/AuthJpaRepository.java
@@ -3,12 +3,15 @@ package com.programmers.smrtstore.domain.auth.infrastructure;
 import com.programmers.smrtstore.domain.auth.domain.entity.Auth;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthJpaRepository extends JpaRepository<Auth, Long> {
 
+    @Query("select a from Auth a where a.username = ?1 and a.user.deletedAt is not null")
     Optional<Auth> findByUsername(String username);
 
     Optional<Auth> findByUserId(Long userId);
+
 }
