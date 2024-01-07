e17e2d368c3d2f88d9dbaa493d6ebc9bed0d2dea
JIN-076 <andy230@naver.com>
2024-01-07 19:11:53 +0900

    [FEAT] @Transactional 어노테이션 추가

--- src/main/java/com/programmers/smrtstore/domain/point/application/PointFacadeImpl.java
+++ src/main/java/com/programmers/smrtstore/domain/point/application/PointFacadeImpl.java
@@ -8,8 +8,10 @@ import com.programmers.smrtstore.domain.point.infrastructure.PointJpaRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PointFacadeImpl implements PointFacade {
 
