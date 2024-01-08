9ef34f81d409bd2e7aedd1460068ba5e57a3ba9a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-01-08 13:23:14 +0900

    [REFACT] findById soft 삭제 적용

--- src/main/java/com/programmers/smrtstore/domain/user/infrastructure/UserRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/user/infrastructure/UserRepository.java
@@ -1,8 +1,13 @@
 package com.programmers.smrtstore.domain.user.infrastructure;
 
 import com.programmers.smrtstore.domain.user.domain.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Override
+    @Query("select u from User u where u.deletedAt is null and u.id = :id")
+    Optional<User> findById(Long id);
 }
