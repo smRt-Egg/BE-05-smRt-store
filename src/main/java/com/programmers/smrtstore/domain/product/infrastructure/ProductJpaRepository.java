c67429eab077d1851add8f523533a54e900eb0fc
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-09 10:24:50 +0900

    [FEAT] Product 검증 로직 추가 (#171)

--- src/main/java/com/programmers/smrtstore/domain/product/infrastructure/ProductJpaRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/product/infrastructure/ProductJpaRepository.java
@@ -1,10 +1,14 @@
 package com.programmers.smrtstore.domain.product.infrastructure;
 
 import com.programmers.smrtstore.domain.product.domain.entity.Product;
+import com.programmers.smrtstore.domain.product.domain.entity.enums.ProductStatusType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
-
+    @Query("select p from Product p where p.id = ?1 and p.productStatusType = ?2")
+    Optional<Product> findByIdAndProductStatusType(Long id, ProductStatusType productStatusType);
 }
