20c82bee298ad54e51ddc506b1fabd5729631617
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-01-10 02:58:26 +0900

    [FEAT] myhome dto 구현

---
+++ src/main/java/com/programmers/smrtstore/domain/user/presentation/dto/res/MyHomeResponse.java
@@ -0,0 +1,47 @@
+package com.programmers.smrtstore.domain.user.presentation.dto.res;
+
+import com.programmers.smrtstore.domain.keep.presentation.dto.res.KeepResponse;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MyHomeResponse {
+
+    private String nickName;
+
+    private String thumbnail;
+
+    private Integer orderDeliveryCount;
+
+    private Integer cartCount;
+
+    private Integer reviewPoint;
+
+    private Integer couponCount;
+
+    List<KeepResponse> allKeeps;
+
+    Integer allKeepsCount;
+
+    List<KeepResponse> clothesKeeps;
+
+    Integer clothesKeepsCount;
+
+    List<KeepResponse> foodKeeps;
+
+    Integer foodKeepsCount;
+
+    List<KeepResponse> electricKeeps;
+
+    Integer electricKeepsCount;
+
+    List<KeepResponse> houseKeeps;
+
+    Integer houseKeepsCount;
+
+    List<KeepResponse> itKeeps;
+
+    Integer itKeepsCount;
+}
