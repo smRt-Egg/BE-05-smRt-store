4c1d468cb2ed5f4da079475b9be8a0d440470de3
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-01-11 05:28:30 +0900

    [FIX] test용 redis config 빈등록 위해 @ActiveProfiles("test") 붙이기

--- src/test/java/com/programmers/smrtstore/SmRtStoreApplicationTests.java
+++ src/test/java/com/programmers/smrtstore/SmRtStoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.programmers.smrtstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmRtStoreApplicationTests {
 
     @Test
