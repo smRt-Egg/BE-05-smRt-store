0b6d986d7da3c99c9db0177bd54490a36d75b5b3
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-01-11 04:50:35 +0900

    [FIX] ci 빌드 오류 수정

--- src/main/java/com/programmers/smrtstore/core/config/RedisConfig.java
+++ src/main/java/com/programmers/smrtstore/core/config/RedisConfig.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
--- src/test/java/com/programmers/smrtstore/core/config/RedisTestConfig.java
+++ src/test/java/com/programmers/smrtstore/core/config/RedisTestConfig.java
@@ -14,43 +14,43 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@Configuration
-@Profile("test")
-public class RedisTestConfig {
-    private static final String REDIS_DOCKER_IMAGE = "redis:latest";
-
-    static {
-        GenericContainer<?> REDIS_CONTAINER =
-            new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-                .withExposedPorts(6379)
-                .withReuse(true);
-
-        REDIS_CONTAINER.start();
-
-        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
-        System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
-    }
-
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    // RedisProperties로 yaml에 저장한 host, post를 연결
-    @Bean
-    public RedisConnectionFactory testRedisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
-    @Bean
-    public RedisTemplate<String, Object> testRedisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(testRedisConnectionFactory());
-
-        return redisTemplate;
-    }
-}
+//@Configuration
+//@Profile("test")
+//public class RedisTestConfig {
+//    private static final String REDIS_DOCKER_IMAGE = "redis:latest";
+//
+//    static {
+//        GenericContainer<?> REDIS_CONTAINER =
+//            new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
+//                .withExposedPorts(6379)
+//                .withReuse(true);
+//
+//        REDIS_CONTAINER.start();
+//
+//        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+//        System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+//    }
+//
+//    @Value("${spring.data.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.data.redis.port}")
+//    private int port;
+//
+//    // RedisProperties로 yaml에 저장한 host, post를 연결
+//    @Bean
+//    public RedisConnectionFactory testRedisConnectionFactory() {
+//        return new LettuceConnectionFactory(host, port);
+//    }
+//
+//    // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
+//    @Bean
+//    public RedisTemplate<String, Object> testRedisTemplate() {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//        redisTemplate.setConnectionFactory(testRedisConnectionFactory());
+//
+//        return redisTemplate;
+//    }
+//}
