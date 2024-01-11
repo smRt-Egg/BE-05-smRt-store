1f16040272456a8cc649a509fd6a25ac389fb4e8
JIN-076 <andy230@naver.com>
2024-01-12 00:33:43 +0900

    [FEAT] 미작성된 리뷰들을 통해 얻을 수 있는 최대 적립금 계산 기능, 작성한 리뷰에 대해 포인트 이력 저장 기능 구현

--- src/main/java/com/programmers/smrtstore/domain/point/domain/entity/enums/PointStatus.java
+++ src/main/java/com/programmers/smrtstore/domain/point/domain/entity/enums/PointStatus.java
@@ -3,6 +3,7 @@ package com.programmers.smrtstore.domain.point.domain.entity.enums;
 public enum PointStatus {
 
     ACCUMULATED,
+    REVIEW_ACCMULATED,
     USED,
     ACCUMULATE_CANCELED,
     USE_CANCELED,
