30359dd03c75ce5cbeadef3c02b45d0af5f45838
SeungSoo So <63526857+voidmelody@users.noreply.github.com>
2024-01-11 22:07:57 +0900

    Feat/#184 review controller 및 미작성 리뷰 갯수 반환 함수 구현 (#195)
    * [FEAT] Review Controller 구현
    
    * [FEAT] 구매했으나 리뷰가 작성되지 않은 리뷰 갯수 도출 함수 구현
    
    * [REFACTOR] productId, userId를 통한 조회 컨트롤러 매핑
    
    * [REFACTOR] @UserId 추가
    
    * [FIX] querydsl 빌드 관련 오류 수정
    
    * [FIX] querydsl 빌드 관련 오류 수정
    
    * [ROLLBACK] build.gradle 롤백
    
    * [REFACTOR] querydsl q 파일 패키지 위치 수정

--- src/main/java/com/programmers/smrtstore/domain/review/infrastructure/ReviewQueryRepository.java
+++ src/main/java/com/programmers/smrtstore/domain/review/infrastructure/ReviewQueryRepository.java
@@ -3,5 +3,5 @@ package com.programmers.smrtstore.domain.review.infrastructure;
 public interface ReviewQueryRepository {
 
     Boolean validateReviewExist(Long userId, Long productId);
-
+    Long getUnWrittenReviewCount(Long userId);
 }
