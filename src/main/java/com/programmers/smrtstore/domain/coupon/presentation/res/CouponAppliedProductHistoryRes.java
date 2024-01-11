cae8798643f61bb2bc6bba21c56b933b1c68a825
YongNyeo <won05121@gmail.com>
2024-01-11 23:33:56 +0900

    [REFACT] 이름변경

--- src/main/java/com/programmers/smrtstore/domain/coupon/presentation/res/CouponAppliedProductHistoryRes.java
+++ src/main/java/com/programmers/smrtstore/domain/coupon/presentation/res/CouponAppliedProductHistoryRes.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class OrderSheetCouponResponse {
+public class CouponAppliedProductHistoryRes {
     private Integer baseAmount;
     private Integer discountAmount;
     private CouponType couponType;
