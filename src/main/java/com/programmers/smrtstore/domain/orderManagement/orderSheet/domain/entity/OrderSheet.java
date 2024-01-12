fd57ffdd21b664accfbe754ca2c647960fb14830
happyjamy <78072370+happyjamy@users.noreply.github.com>
2024-01-13 00:48:02 +0900

    [FEAT] OrderSheet 작성자 검증 로직 추가

--- src/main/java/com/programmers/smrtstore/domain/orderManagement/orderSheet/domain/entity/OrderSheet.java
+++ src/main/java/com/programmers/smrtstore/domain/orderManagement/orderSheet/domain/entity/OrderSheet.java
@@ -1,5 +1,8 @@
 package com.programmers.smrtstore.domain.orderManagement.orderSheet.domain.entity;
 
+import static com.programmers.smrtstore.core.properties.ErrorCode.INVALID_USER;
+
+import com.programmers.smrtstore.domain.orderManagement.orderSheet.exception.OrderSheetException;
 import com.programmers.smrtstore.domain.orderManagement.orderedProduct.domain.entity.OrderedProduct;
 import com.programmers.smrtstore.domain.user.domain.entity.User;
 import jakarta.persistence.CascadeType;
@@ -14,8 +17,10 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -35,11 +40,29 @@ public class OrderSheet {
     @JoinColumn(name = "user_id", updatable = false)
     private User user;
 
+    // TODO: 연관관계 매핑
     @OneToMany(mappedBy = "orderSheet", cascade = CascadeType.ALL)
-    private List<OrderedProduct> orderedProducts;
+    private List<OrderedProduct> orderedProducts = new ArrayList<>();
 
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Builder
+    public OrderSheet(
+        Long id, User user, List<OrderedProduct> orderedProducts, LocalDateTime createdAt
+    ) {
+        this.id = id;
+        this.user = user;
+        this.orderedProducts = orderedProducts;
+        orderedProducts.forEach(orderedProduct -> orderedProduct.setOrderSheet(this));
+        this.createdAt = createdAt;
+    }
+
+    public void validateOwnerOfOrderSheet(User user) {
+        if (!this.user.equals(user)) {
+            throw new OrderSheetException(INVALID_USER, String.valueOf(user.getId()));
+        }
+    }
 
 }
