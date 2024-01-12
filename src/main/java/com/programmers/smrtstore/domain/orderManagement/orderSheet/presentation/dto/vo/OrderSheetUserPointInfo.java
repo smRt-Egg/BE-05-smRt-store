d1b5df49ce38fcc79c452bf276eb34f1dc934cac
happyjamy <78072370+happyjamy@users.noreply.github.com>
2024-01-13 00:52:21 +0900

    [FEAT] OrderSheet 생성,조회 서비스 구현

--- src/main/java/com/programmers/smrtstore/domain/orderManagement/orderSheet/presentation/dto/vo/OrderSheetUserPointInfo.java
+++ src/main/java/com/programmers/smrtstore/domain/orderManagement/orderSheet/presentation/dto/vo/OrderSheetUserPointInfo.java
@@ -1,8 +1,9 @@
 package com.programmers.smrtstore.domain.orderManagement.orderSheet.presentation.dto.vo;
 
-import com.programmers.smrtstore.domain.point.application.dto.res.OrderExpectedPointDto;
-
 public class OrderSheetUserPointInfo {
     private Integer pointBalance;
 
+    public OrderSheetUserPointInfo(Integer pointBalance) {
+        this.pointBalance = pointBalance;
+    }
 }
