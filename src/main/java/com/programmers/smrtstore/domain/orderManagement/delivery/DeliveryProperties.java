2e79feabaa15a39d887ebe54cf0efbfe168edf2e
happyjamy <78072370+happyjamy@users.noreply.github.com>
2024-01-13 00:41:58 +0900

    [FEAT] 배송비 관련 상수 관리

---
+++ src/main/java/com/programmers/smrtstore/domain/orderManagement/delivery/DeliveryProperties.java
@@ -0,0 +1,9 @@
+package com.programmers.smrtstore.domain.orderManagement.delivery;
+
+public class DeliveryProperties {
+
+    public static final int STORE_DELIVERY_FEE = 3000;
+
+    private DeliveryProperties() {
+    }
+}
